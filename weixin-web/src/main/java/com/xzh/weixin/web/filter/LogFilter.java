99a6618b39e54bfbcd09332fa4f280f2ab60a288
weiawang <weiawang@qiyi.com>
2018-05-14 14:13:00 +0800

    author

---
+++ weixin-web/src/main/java/com/xzh/weixin/web/filter/LogFilter.java
@@ -0,0 +1,48 @@
+package com.xzh.weixin.web.filter;
+
+import com.alibaba.fastjson.JSON;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@WebFilter
+public class LogFilter implements Filter {
+
+    private final static Logger logger = LoggerFactory.getLogger(LogFilter.class);
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        StringBuilder stringBuilder = new StringBuilder(req.getRequestURI()).append(" ");
+        try {
+            Map<String, String[]> parameterMap = req.getParameterMap();
+            String params = JSON.toJSONString(parameterMap);
+
+            stringBuilder.append(params);
+            logger.info(stringBuilder.toString());
+            chain.doFilter(request, response);
+        } catch (Exception e) {
+            logger.error(stringBuilder.toString(), e);
+        }
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
